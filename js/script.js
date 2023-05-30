993bf81a980f6ae843597f8dd392a892a7b246a5
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-05-30 20:22:27 +0200

    fix ticket

--- js/script.js
+++ js/script.js
@@ -1,58 +1,3 @@
-// // prendo l'elemento dal DOM
-// const totalElement = document.getElementById('total');
-
-// // preparo il messaggio per l'utente
-// const priceMessage = 'Il prezzo del tuo biglietto è: €';
-
-// // prezzo al km
-// const pricePerKM = 0.21;
-
-// // preparo eventuale sconto
-// let discount = null;
-
-// // raccolgo info utente
-// const kms = parseInt(prompt('Quanti km devi percorrere?' , 10).trim());
-// const age = parseInt(prompt('Quanti anni hai?' , 30).trim());
-// console.log(kms , age);
-
-// //****/ validazione*****
-// if (isNaN(kms) || isNaN(age) || age <= 0 || kms <= 0){
-//     alert('I dati non sono validi');
-// } else{
-
-//     // calcolo prezzo
-//     const basePrice = pricePerKM * kms;
-//     let finalPrice = basePrice;
-    
-//     // valuto eventuale sconto
-//     if(age >= 65) discount = 40;
-//     else if(age <= 18) discount = 20;
-
-//     if(discount){
-//         // recupero elemento sconto
-//         const discountElement = document.getElementById('discount-message');
-
-//         // preparo messaggio di sconto
-//         const discountMessage = `Hai ricevuto uno sconto del ${discount}%`;
-
-//         // inserisco testo nell'elemento
-//         discountElement.innerText = discountMessage;
-
-//         // calcolo sconto
-//         finalPrice -= (finalPrice / 100) * discount;
-//     }
-
-//     // inserisco prezzo nell'elemento in pagina
-//     totalElement.innerText = priceMessage + finalPrice.toFixed(2);
-
-//     // aggiungo prezzo scontato
-//     if(discount){
-//         totalElement.innerHTML += ` (<del class="text-danger">€${basePrice}</del>)`;
-//     }
-// }
-
-
-// **************
 
 // form
 const nameUser = document.getElementById('name');
@@ -65,3 +10,38 @@ const ticketSection = document.getElementById('ticket-section');
 const passengerEl = document.getElementById('passenger');
 const rateEl = document.getElementById('rate');
 const priceEl = document.getElementById('price');
+
+// Al click del bottone
+
+createButton.addEventListener('click' , function(){
+    const nameValue = nameUser.value.trim();
+    const kmValue = parseInt(kmUser.value);
+    const ageValue = ageUser.value;
+
+    // validazione
+    if(!nameValue || isNaN(kmValue) || kmValue < 1){
+        return;
+    }
+
+    // prezzo e tariffa di base
+    let price = kmValue * 0.21;
+    let rateName = 'Tariffa Ordinaria';
+
+    if(ageValue === 'min'){
+        price *= 0.8;
+        rateName = 'Tariffa Giovani'
+    }
+    else if(ageValue === 'over'){
+        price *= 0.6;
+        rateName = 'Tariffa Over 65'
+    }
+
+    // inserisco i dati nel biglietto
+    passengerEl.innerText = nameValue;
+    rateEl.innerText = rateName;
+    priceEl.innerText = '€' + price.toFixed(2);
+
+    // faccio comparire il biglietto
+    ticketSection.classList.remove('d-none');
+
+})
